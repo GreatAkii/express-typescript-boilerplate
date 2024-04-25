d89f00b3187232558d66431d6e0e4ce24cd79c7e
GreatAkii <ashinde266@gmail.com>
2024-04-25 22:27:27 +0100

    Add MySQL connection and query to retrieve data from the database

--- dist/index.js
+++ dist/index.js
@@ -4,12 +4,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mysql2_1 = __importDefault(require("mysql2"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-console.log(process.env.APP_PORT);
-console.log(typeof process.env.DB_PASSWORD);
 const app = (0, express_1.default)();
+const connection = mysql2_1.default.createConnection({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'squid',
+});
 app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+    connection.query('SELECT * FROM squid.businesses', (err, results) => {
+        if (err) {
+            return res.status(500).json({ message: 'Internal Server Error' });
+        }
+        res.json(results);
+    });
+});
 const port = process.env.APP_PORT ? parseInt(process.env.APP_PORT) : 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}...`);
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
 import express, { Request, Response } from 'express';
+import mysql from 'mysql2';
 import dotenv from 'dotenv';
 
 dotenv.config();
-console.log(process.env.APP_PORT);
-console.log(typeof process.env.DB_PASSWORD);
 const app = express();
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'squid',
+});
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  connection.query('SELECT * FROM squid.businesses', (err, results) => {
+    if (err) {
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
+    res.json(results);
+  });
+});
 
 
 const port: number = process.env.APP_PORT ? parseInt(process.env.APP_PORT) : 3000;
