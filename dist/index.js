c34fef5542dcdeede359ce209af48f9ecec3a4d6
GreatAkii <ashinde266@gmail.com>
2024-04-25 21:26:03 +0100

    Add dotenv for secrets

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+console.log(process.env.APP_PORT);
+console.log(typeof process.env.DB_PASSWORD);
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+const port = process.env.APP_PORT ? parseInt(process.env.APP_PORT) : 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}...`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config();
+console.log(process.env.APP_PORT);
+console.log(typeof process.env.DB_PASSWORD);
 const app = express();
 
 app.use(express.json());
 
-const port: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+const port: number = process.env.APP_PORT ? parseInt(process.env.APP_PORT) : 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
 });
