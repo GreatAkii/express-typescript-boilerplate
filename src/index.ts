fd1cc93352eb589d98ddd23e75e399ce352d5bcc
GreatAkii <ashinde266@gmail.com>
2024-04-26 21:27:04 +0100

    Update logging middleware and server configuration in index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,16 @@ dotenv.config();
 const app: Express = express();
 const database = new db();
 
+// Enable JSON parsing middleware
 app.use(express.json());
+
+// Enable URL-encoded form data parsing middleware
 app.use(express.urlencoded({ extended: true }));
 
+// Log the current environment mode
 console.log(`> running in ${app.get("env")} mode`);
+
+// Enable logging middleware in development mode
 if (app.get("env") === "development") {
   const logger = pino();
   app.use(pinoHttp({ logger }));
@@ -24,13 +30,16 @@ app.get("/discovery", (req: Request, res: Response) => {
       res.json(results);
     })
     .catch((err) => {
-      res.status(500).json({ error: "Error retrieving data from database" });
+      res.status(500).json("Error: " + err.message);
     });
 });
 
+// Set the port number for the server
 const port: number = process.env.APP_PORT
   ? parseInt(process.env.APP_PORT)
   : 3000;
+
+// Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`Server listening on port ${port}...`);
 });
